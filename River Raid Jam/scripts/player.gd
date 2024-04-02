f1a7a5dcd5c8f458b3769ec4c817b47337a60f32
JulioEvencio <julioigreja.dev@gmail.com>
2024-04-01 22:45:07 -0300

    added: script base for player

---
+++ River Raid Jam/scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const SPEED : int = 50
+
+func _physics_process(_delta : float) -> void:
+	move()
+
+func move() -> void:
+	var speed_y : int = -20
+	
+	if Input.is_action_pressed("move_down"):
+		speed_y = -5
+	elif Input.is_action_pressed("move_up"):
+		speed_y -= SPEED
+	
+	velocity.y = speed_y
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	move_and_slide()
