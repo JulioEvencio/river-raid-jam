c50bb509a7390503c043a40149514f13a62d16f3
JulioEvencio <julioigreja.dev@gmail.com>
2024-04-04 18:14:50 -0300

    added: resource for button

--- River Raid Jam/scripts/screens/menu.gd
+++ River Raid Jam/scripts/screens/menu.gd
@@ -6,11 +6,11 @@ func _physics_process(_delta : float) -> void:
 	background.motion_offset.x += 1
 	background.motion_offset.y += 1
 
-func _on_start_pressed() -> void:
+func _on_start_pressed():
 	Transition.start(func(): get_tree().change_scene_to_file("res://scenes/levels/level.tscn"))
 
-func _on_credits_pressed() -> void:
+func _on_credits_pressed():
 	print("Credits")
 
-func _on_exit_pressed() -> void:
+func _on_exit_pressed():
 	get_tree().quit()
