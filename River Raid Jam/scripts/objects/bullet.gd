5dbd8da88970c2f4b8744cad0333b15c58102e99
JulioEvencio <julioigreja.dev@gmail.com>
2024-04-06 18:42:43 -0300

    refactor: update type for bullet

--- River Raid Jam/scripts/objects/bullet.gd
+++ River Raid Jam/scripts/objects/bullet.gd
@@ -1,16 +1,17 @@
-extends Area2D
+extends CharacterBody2D
 class_name Bullet
 
 const SPEED : int = -5
 
 func _physics_process(_delta : float) -> void:
-	position.y += SPEED
+	velocity.y = SPEED
+	move_and_collide(Vector2(0, SPEED))
 
-func add_score():
+func add_score() -> void:
 	if Singleton.score < 9999999999:
 		Singleton.score += 1
 
-func _on_body_entered(body) -> void:
+func _on_area_2d_body_entered(body) -> void:
 	if body is Enemy:
 		add_score()
 		body.to_kill()
